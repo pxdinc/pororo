670cdb7cf706ce4d78e13485d3bfc258541aafbf
gunggmee <66834751+gunggmee@users.noreply.github.com>
2021-07-22 13:22:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 
 packages = find_packages()
 requirements = [
-    "torch==1.9.0",
+    # "torch==1.9.0",
+    "torch",
     "torchvision>=0.7.0",
     "pillow>=4.1.1",
     # 0.10.2 doesn't work on python 3.9
